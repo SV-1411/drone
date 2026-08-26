1d3c8309401d2c7e103dd81f37a53257ad3c6af3
Shivansh Verma <shivansh1411@gmail.com>
2026-08-26 14:34:56 +0530

    Point drone sim at GCS flight simulator

--- hub/drone_sim_page.py
+++ hub/drone_sim_page.py
@@ -1,6 +1,6 @@
-"""Compatibility route for the VanniKawachh geographic flight viewer."""
+"""Compatibility route for the VanniKawachh GCS geographic flight simulator."""
 from fastapi.responses import HTMLResponse
-from .drone_flight_clean import HTML
+from .drone_flight_gcs import HTML
 
 
 def attach(app):
